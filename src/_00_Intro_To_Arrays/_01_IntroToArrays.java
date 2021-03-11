51dbb5bbc2aad8d8675a647ac2fe1b52e57d4d57
michellebazan <24michelle.b@gmail.com>
2021-03-10 17:25:10 -0800

    robot goes brrr

--- src/_00_Intro_To_Arrays/_01_IntroToArrays.java
+++ src/_00_Intro_To_Arrays/_01_IntroToArrays.java
@@ -3,34 +3,69 @@ package _00_Intro_To_Arrays;
 import java.util.Random;
 
 public class _01_IntroToArrays {
-    public static void main(String[] args) {
-        // 1. declare and Initialize an array 5 Strings
+	public static void main(String[] args) {
+		// 1. declare and Initialize an array 5 Strings
+		String[] hu = new String[5];
 
-        // 2. print the third element in the array
+		// 2. print the third element in the array
+		System.out.println(hu[2]);
+		// 3. set the third element to a different value
+		hu[2] = "tao";
 
-        // 3. set the third element to a different value
+		hu[0] = "hu";
+		hu[1] = "jean";
+		hu[3] = "barba";
+		hu[4] = "xianling";
+		// 4. print the third element again
+		System.out.println(hu[2]);
+		// 5. use a for loop to set all the elements in the array to a string
+		// of your choice
+		int c = 0;
+		while (c < 5) {
+			hu[c] = "yayayyayayayaya";
+			System.out.println(hu[c]);
+			c++;
+		}
+		// 6. use a for loop to print all the values in the array
+		// BE SURE TO USE THE ARRAY'S length VARIABLE
+		// for (statement 1; statement 2; statement 3) {
+		// code block to be executed
+		// }
+		for (int i = 0; i < 5; i++) {
+			System.out.println(hu[i].length());
+			System.out.println(hu[i]);
+		}
+		// 7. make an array of 50 integers
+		int[] ints = new int[50];
+		// 8. use a for loop to make every value of the integer array a random
+		// number
+		Random ran = new Random();
+		for (int i = 0; i < 50; i++) {
+			ints[i] = (ran.nextInt(100));
+		}
+		// 9. without printing the entire array, print only the smallest number
+		// on the array
+		int small = ints[0];
+		for (int i = 0; i < 49; i++) {
+			if (small > ints[i + 1]) {
+				small = ints[i + 1];
+			}
 
-        // 4. print the third element again
-
-        // 5. use a for loop to set all the elements in the array to a string
-        //    of your choice
-
-        // 6. use a for loop to print all the values in the array
-        //    BE SURE TO USE THE ARRAY'S length VARIABLE
-
-        // 7. make an array of 50 integers
-
-        // 8. use a for loop to make every value of the integer array a random
-        //    number
-
-        // 9. without printing the entire array, print only the smallest number
-        //    on the array
-
-        // 10 print the entire array to see if step 8 was correct
-
-        // 11. print the largest number in the array.
-
-        // 12. print only the last element in the array
-
-    }
+		}
+		System.out.println("The smallest is " + small);
+		// 10 print the entire array to see if step 8 was correct
+		for (int i = 0; i < 50; i++) {
+			System.out.println(ints[i]);
+		}
+		// 11. print the largest number in the array.
+		int large = ints[0];
+		for (int i = 0; i < 49; i++) {
+			if (large < ints[i + 1]) {
+				large = ints[i + 1];
+			}
+		}
+		System.out.println("the largest is " + large);
+		// 12. print only the last element in the array
+		System.out.println(ints[49]);
+	}
 }
--- src/_00_Intro_To_Arrays/_02_RobotRace.java
+++ src/_00_Intro_To_Arrays/_02_RobotRace.java
@@ -9,28 +9,103 @@ import org.jointheleague.graphical.robot.Robot;
 public class _02_RobotRace {
 	// 1. make a main method
 	public static void main(String[] args) {
+		// 2. create an array of 5 robots.
+		
 		Robot[] ro = new Robot[5];
+		// 3. use a for loop to initialize the robots.
 		for(int i = 0; i < 5; i++) {
-			//ro[i].
+			ro[i] = new Robot();
+			// 4. make each robot start at the bottom of the screen, side by side, facing up
+			ro[i].setX(i * 60 + 30);
+			ro[i].setY(600);
+			ro[i].setSpeed(69);
 		}
-	}
-
-	// 2. create an array of 5 robots.
-
-	// 3. use a for loop to initialize the robots.
-
-	// 4. make each robot start at the bottom of the screen, side by side, facing up
-
-	// 5. use another for loop to iterate through the array and make each robot move
-	// a random amount less than 50.
-
-	// 6. use a while loop to repeat step 5 until a robot has reached the top of the
-	// screen.
-
-	// 7. declare that robot the winner and throw it a party!
+		Random rand = new Random();
+		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+		// screen.
+		int ha = 0;
+		while(ha == 0) {
+			
+			// 5. use another for loop to iterate through the array and make each robot move
+			// a random amount less than 50.
+			for(int k = 0; k < 5; k++) {
+				ro[k].move( rand.nextInt(50));
+				if(ro[k].getY()<=0) {
+					ha = 1;
+					// 7. declare that robot the winner and throw it a party!
+					System.out.println( "Robot #" + k + " is the winner!!!");
+					ro[k].sparkle();
+					k=5;
+					for(int a=0; a < 5;a++) {
+						ro[a].turn(360+90);
+						ro[a].moveTo(0, a*60 + 20);
+					}
+				}
+			}
+			
+			
+		}
+		
+		
+		int ae = 0;
+		while(ae == 0) {
+			for(int f = 0; f < 5; f++) {
+				ro[f].move( rand.nextInt(50));
+				if(ro[f].getX()>=900) {
+					ae = 1;
+					// 7. declare that robot the winner and throw it a party!
+					System.out.println( "Robot #" + f + " is the winner!!!");
+					ro[f].sparkle();
+					f=5;
+				}
+		    }
+		
+	    }
+		
+		for(int i = 0; i < 5; i++) {
+			// 4. make each robot start at the bottom of the screen, side by side, facing up
+			ro[i].moveTo(0 + i * 60, 600);
+		}
+		int circle = 0;
+		int chicken = 0;
+		while(circle ==0) {
+			for(int q = 0; q < 5; q++) {
+				ro[q].move( rand.nextInt(50));
+				if(ro[q].getY()<=0) {
+					// 7. declare that robot the winner and throw it a party!
+					chicken++;
+					ro[q].sparkle();
+					
+					for(int a=0; a < 5;a++) {
+						ro[a].turn(360+90);
+						ro[a].moveTo(ro[q].getX() + 60 * a, ro[q].getY());
+					}
+					q=5;
+				}
+			}
+			for(int z = 0; z < 5; z++) {
+				ro[z].move( rand.nextInt(50));
+				if(ro[z].getX()>=900) {
+					chicken++;
+					
+					for(int w=0; w < 5;w++) {
+						ro[w].turn(360+90);
+						ro[w].moveTo(ro[z].getX() , ro[z].getY());
+					}
+					
+					if(chicken >=4) {
+						circle = 1;
+						System.out.println( "Robot #" + z + " is the winner!!!");
+						ro[z].sparkle();
+					}
+					z=5;
+				}
+		    }
+		}
 
 	// 8. try different races with different amounts of robots.
 
 	// 9. make the robots race around a circular track.
 
 }
+	}
